49e15ee58db271588174a6367d31bda485b59e10
user <user@usernoMacBook-Pro.local>
2025-04-05 20:53:18 +0800

    mmmm

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@ const config = {
     prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT,
     imageKit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOOINT,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     },
     databaseUrl: process.env.DATABASE_URL,
