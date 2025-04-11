691bba229c1983d587dd2a7d12ad29a7fd673465
user <user@usernoMacBook-Pro.local>
2025-04-11 19:58:21 +0800

    auth.ts fixed

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { users } from "@/database/schema";
 import { db } from "@/database/drizzle";
@@ -6,17 +6,17 @@ import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-import ratelimit from "../ratelimit";
+import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
-import { workflowClient } from "../workflow";
+import { workflowClient } from "@/lib/workflow";
 import  config from "@/lib/config";
 
 
  
-export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">) => {
+export const signInWithCredentials = async (params: Pick<AuthCredentials, "email" | "password">, ) => {
   const { email, password } = params;
 
-  const ip = ((await headers()).get("x-forwarded-for") || "127.0.0.1");
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
   const { success } = await ratelimit.limit(ip);
   if (!success) return redirect("/too-fast");
   
@@ -31,8 +31,7 @@ export const signInWithCredentials = async (params: Pick<AuthCredentials, "email
       return { success: false, error: result.error };
     };
     return { success: true };
-  } 
-  catch (error) {
+  } catch (error) {
     console.log(error, "Signin error");
     return { success: false, error: "Signin error" };
   }
@@ -42,12 +41,12 @@ export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, universityId, password, universityCard } = params;
   //check if the user already exist
 
-  const ip = ((await headers()).get("x-forwarded-for") || "127.0.0.1");
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
   const { success } = await ratelimit.limit(ip);
   if (!success) return redirect("/too-fast");
   const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
   if (existingUser.length > 0) {
-    return { success: false, error: "User already exists"};
+    return { success: false, error: "User already exists" };
   }
 
   const hashedPassword = await hash(password, 10);
@@ -60,6 +59,7 @@ export const signUp = async (params: AuthCredentials) => {
         fullName,
       },
     });
+    await signInWithCredentials({ email, password });
     return { success: true }; 
   } catch (error) {
     console.log(error, "Signup error"); 
