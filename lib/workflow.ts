88533cd6f4411328a76b49dc0a4b851ed7a52ced
user <user@usernoMacBook-Pro.local>
2025-04-12 19:09:23 +0800

    lib workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,27 +1,37 @@
-import config from "./config"
-import { Client as WorkFlowClient } from "@upstash/workflow"
+import { Client as WorkflowClient } from "@upstash/workflow";
 import { Client as QStashClient, resend } from "@upstash/qstash";
+import config from "@/lib/config";
 
-export const workflowClient = new WorkFlowClient({
+export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
 
-
-const qstashClient= new QStashClient({ token: config.env.upstash.qstashToken });
-
-export const sendEmail = async ({ email, subject, message }: { email: string, subject: string, message: string}) => {
-  await qstashClient.publishJSON({
-  api: {
-    name: "email",
-    provider: resend({ token: config.env.resendToken }),
-  },
-  body: {
-    from: "My Library <teknik09@ican09.com>",
-    to: [email],
-    subject,
-    html: message,
-  },
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
 });
-}
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "My Library <teknik09@ican09.com>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
+
 
