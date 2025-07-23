7391176e9b05cab2bd983bd37f4b7cc3a3b83fdb
user <user@usernoMacBook-Pro.local>
2025-07-23 19:38:21 +0800

    fixed borrow button

--- components/ui/BorrowBtn.tsx
+++ components/ui/BorrowBtn.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Image from "next/image"
-import { Button } from "./button"
+import Image from "next/image";
+import { Button } from "./button";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -13,16 +13,23 @@ interface Props {
   borrowingEligibility: {
     isEligible: boolean;
     message: string;
-  }
+  };
 }
 
-export default function BorrowBtn({bookId, userId, borrowingEligibility: { isEligible, message }}: Props) {
+export default function BorrowBtn({
+  bookId,
+  userId,
+  borrowingEligibility: { isEligible, message },
+}: Props) {
   const router = useRouter();
   const [borrowing, setBorrowing] = useState(false);
+
   const handleBorrow = async () => {
     if (!isEligible) {
-      toast.error(`${message}`);
+      toast.error(message);
+      return;
     }
+
     setBorrowing(true);
     try {
       const result = await borrowBook({ bookId, userId });
@@ -32,18 +39,25 @@ export default function BorrowBtn({bookId, userId, borrowingEligibility: { isEli
       } else {
         toast.error(result.error);
       }
-    } catch(error) {
+    } catch (error) {
       toast.error("An error occurred while borrowing the book");
     } finally {
       setBorrowing(false);
     }
-  }
+  };
+
   return (
     <div>
-      <Button className="book-overview_btn" onClick={handleBorrow} disabled={borrowing}>
-          <Image src="/icons/book.svg" alt="book" width={20} height={20}/>
-          <p className="font-bebas-neue text-xl text-dark-100">{borrowing ? "Borrowing ..." : "Borrow Book"}</p>
-        </Button>  
+      <Button
+        className="book-overview_btn"
+        onClick={handleBorrow}
+        disabled={borrowing}
+      >
+        <Image src="/icons/book.svg" alt="book" width={20} height={20} />
+        <p className="font-bebas-neue text-xl text-dark-100">
+          {borrowing ? "Borrowing ..." : "Borrow Book"}
+        </p>
+      </Button>
     </div>
-  )
+  );
 }
