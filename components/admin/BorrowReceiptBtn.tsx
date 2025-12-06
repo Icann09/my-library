5c8815958e1d9fce101aed8d70e2d8f85855b654
Ican09 <kaisanfarasdag09@gmail.com>
2025-12-06 22:40:50 +0800

    jajajj

--- components/admin/BorrowReceiptBtn.tsx
+++ components/admin/BorrowReceiptBtn.tsx
@@ -27,12 +27,23 @@ export default function BorrowReceiptBtn({
     if (generated || loading) return;
 
     setLoading(true);
-    await sendEmail({ email, subject: "This is your receipt", message: bookBorrowReceipt(receipt) });
 
-    const markResult = await markReceiptGenerated(borrowId);
-    console.log("🔧 Marked as generated:", markResult);
-    setGenerated(true);
-    setLoading(false);
+    try {
+    
+      // await sendEmail({ email, subject: "This is", message: bookBorrowReceipt(receipt) });
+
+      const markResult = await markReceiptGenerated(borrowId);
+      console.log("🔧 Marked as generated:", markResult);
+
+      setGenerated(true);
+    } catch (error: any) {
+      console.error("❌ Receipt generation failed:");
+      console.error("Error message:", error?.message);
+      console.error("Full error:", error);
+
+      alert(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
