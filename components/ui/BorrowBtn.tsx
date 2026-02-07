619ae3510fc3fff8dad4afd3f3cabfc18b548215
Ican09 <kaisanfarasdag09@gmail.com>
2026-02-07 18:52:49 +0800

    refactor(borrow-btn): use useTransition for server action

--- components/ui/BorrowBtn.tsx
+++ components/ui/BorrowBtn.tsx
@@ -2,43 +2,41 @@
 
 import Image from "next/image";
 import { Button } from "./button";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { toast } from "sonner";
 import { borrowBook } from "@/lib/actions/book";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function BorrowBtn({ bookId }: { bookId: string }) {
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
-  const [borrowing, setBorrowing] = useState(false);
 
-  const handleBorrow = async () => {
-    setBorrowing(true);
-
-    try {
-      const result = await borrowBook(bookId);
-
+  const handleBorrow = () => {
+    startTransition(async () => {
+      try {
+        const result = await borrowBook(bookId);
       if (result.success) {
-        toast.success("Book borrowed successfully");
+        toast.success("Book borrowed succesfully");
         router.push("/my-profile");
       } else {
         toast.error(result.error);
       }
-    } catch {
-      toast.error("An error occurred while borrowing the book");
-    } finally {
-      setBorrowing(false);
-    }
-  };
+      } catch {
+        toast.error("Something went wrong. Please try again!");
+      }
+    })
+  }
 
   return (
     <Button
       className="book-overview_btn"
       onClick={handleBorrow}
-      disabled={borrowing}
+      disabled={isPending}
     >
       <Image src="/icons/book.svg" alt="book" width={20} height={20} />
       <p className="font-bebas-neue text-xl text-dark-100">
-        {borrowing ? "Borrowing ..." : "Borrow Book Request"}
+        {isPending ? "Borrowing ..." : "Borrow Book Request"}
       </p>
     </Button>
   );
